e5f261ec7713384ce4f53a8ded51f596cf5821bc
Benjamin Riggs <deisum@gmail.com>
2015-02-24 18:35:07 -0600

    Type problems. Missed a detail in the stuff I blatantly copied.

--- pfodb/urls.py
+++ pfodb/urls.py
@@ -6,6 +6,6 @@ from .views import *
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pfotools.views.home', name='home'),
-    url(r'^$', Industry_Index, name='industry_index'),
-    url(r'^/items/(?P<query>\w+)/(?P<plus_value>\w+)$', Item, name='items'),
+    url(r'^$', Industry_Index.as_view(), name='industry_index'),
+    url(r'^/items/(?P<query>\w+)/(?P<plus_value>\w+)$', Item.as_view(), name='items'),
 )
