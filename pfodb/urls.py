ed4e955f36268fcec1d355c98071ddc39baf9d18
Benjamin Riggs <deisum@gmail.com>
2015-02-25 04:19:11 -0600

    Improved URLs, fixed plus_value stuff, added feat required.

--- pfodb/urls.py
+++ pfodb/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pfotools.views.home', name='home'),
     url(r'^$', Industry_Index.as_view(), name='industry_index'),
-    url(r'''^crafting(/(?P<query>[\w ']+))?$''', Item.as_view(), name='items'),
-    #url(r'''^crafting(/(?P<query>[\w ']+)(\w+\+(?P<plus_value>\d))?)?$''', Item.as_view(), name='items'),
-    url(r'''^refining(/(?P<query>[\w ']+))?$''', Component.as_view(), name='items'),
-    #url(r'''^refining(/(?P<query>[\w ']+)(\w+\+(?P<plus_value>\d))?)?$''', Component.as_view(), name='items'),
+    url(r'''^crafting/?((?<=/)(?P<query>[\w ']+))?$''', Item.as_view(), name='items'),
+    #url(r'''^crafting(/(?P<query>[\w ']+)(\+(?P<plus_value>\d))?)?$''', Item.as_view(), name='items'),
+    #url(r'''^refining(/(?P<query>[\w ']+))?$''', Component.as_view(), name='items'),
+    url(r'''^refining/?((?<=/)(?P<query>[\w ']+)(\+(?P<plus_value>\d))?)?$''', Component.as_view(), name='items'),
 )
--- pfodb/views.py
+++ pfodb/views.py
@@ -18,28 +18,34 @@ class Item(View):
 
     def get(self, request, *args, **kwargs):
         query = kwargs.get('query')
-        if query:
-            return HttpResponse(
-                "<br>".join(
-                    "{name} +0: {ingredients}".format(
-                        name=item.name, ingredients=", ".join(
-                            '{quantity} {name}'.format(quantity=ingredient.quantity, name=ingredient.material.name)
-                            for ingredient in item.recipe.bill_of_materials.all()))
-                    for item in models.Item.objects.filter(name__icontains=query)))
-        return HttpResponse('Search for items in the url, e.g. crafting/bow')
+        if not query:
+            return HttpResponse('Search for items in the url, e.g. crafting/bow')
+        filter_kwargs = dict(name__icontains=query.strip())
+        return HttpResponse(
+            "<br>".join(
+                "{name} ({feat}): {ingredients}".format(
+                    name=item.name, feat=item.recipe.required_feat,
+                    ingredients=", ".join(
+                        '{quantity} {name}'.format(quantity=ingredient.quantity, name=ingredient.material.name)
+                        for ingredient in item.recipe.bill_of_materials.all()))
+                for item in models.Item.objects.filter(**filter_kwargs)))
 
 
 class Component(View):
 
     def get(self, request, *args, **kwargs):
         query = kwargs.get('query')
+        if not query:
+            return HttpResponse('Search for items in the url, e.g. refining/oak')
+        filter_kwargs = dict(name__icontains=query.strip())
         plus_value = kwargs.get('plus_value')
-        if query:
-            return HttpResponse(
-                "<br>".join(
-                    "{name} +{plus_value}: {ingredients}".format(
-                        name=item.name, plus_value=item.plus_value, ingredients=", ".join(
-                            '{quantity} {name}'.format(quantity=ingredient.quantity, name=ingredient.material.name)
-                            for ingredient in item.recipe.bill_of_materials.all()))
-                    for item in models.Component.objects.filter(name__icontains=query)))
-        return HttpResponse('Search for items in the url, e.g. refining/oak')
+        if plus_value is not None:
+            filter_kwargs['plus_value'] = int(plus_value)
+        return HttpResponse(
+            "<br>".join(
+                "{name} +{plus_value} ({feat}): {ingredients}".format(
+                    name=item.name, plus_value=item.plus_value, feat=item.recipe.required_feat,
+                    ingredients=", ".join(
+                        '{quantity} {name}'.format(quantity=ingredient.quantity, name=ingredient.material.name)
+                        for ingredient in item.recipe.bill_of_materials.all()))
+                for item in models.Component.objects.filter(**filter_kwargs)))
